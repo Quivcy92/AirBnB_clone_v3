94539464804ff4ab26aa64789b64dcf5a9098a63
Alhassan Zainab <azainab390@gmail.com>
2023-11-05 01:43:52 -0800

    0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""app.py"""
+"""Flask App that integrates with AirBnB static HTML Template"""
 from models import storage
 from api.v1.views import app_views
 from werkzeug.exceptions import HTTPException
